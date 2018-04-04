6a67dd74a0ec71b0f66570c0dc23c3aeeb2d266e
Elesmer <paelesmer@gmail.com>
2018-04-04 23:19:22 +0300

    Add table, change styles.

--- app/js/common.js
+++ app/js/common.js
@@ -3,12 +3,16 @@ jQuery(document).ready(function() {
 	// Sidenav init
 
 	$('.button-collapse').sideNav({
-		menuWidth: 350, // Default is 300
+		menuWidth: 300, // Default is 300
 		edge: 'left', // Choose the horizontal origin
 		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
 		draggable: true, // Choose whether you can drag to open on touch screens,
 		onOpen: function(el) {}
 	});
+
+	// Dropdown init
+
+
 
 	/* Wow */
 
