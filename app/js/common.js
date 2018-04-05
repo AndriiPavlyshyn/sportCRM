ea42fae8471a180b05d352f233e842480d96bf6f
Elesmer <paelesmer@gmail.com>
2018-04-05 16:37:12 +0300

    Flter complete

--- app/js/common.js
+++ app/js/common.js
@@ -10,9 +10,29 @@ jQuery(document).ready(function() {
 		onOpen: function(el) {}
 	});
 
-	// Dropdown init
+	// Datapicker init
+
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15, // Creates a dropdown of 15 years to control year,
+		today: 'Сегодня',
+		clear: 'Отменить',
+		close: 'ок',
+		closeOnSelect: false,
+		monthsFull: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+		monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Ию', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+		weekdays: ['Воскресенье', 'Понидельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+		weekdaysShort: ['Вс', 'Пон', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+	});
+
+	$('.dropdown-button + .dropdown-content').on('click', function(event) {
+		event.stopPropagation();
+	});
 
 
+	// Select
+
+	$('select').material_select();
 
 	/* Wow */
 
